d1671d0a5b6482bf966dbefbeb14cb6507aae459
lwyers2 <lwyers2@gmail.com>
2025-01-20 22:13:03 +0000

    going back to basics

--- backend/controllers/student.js
+++ backend/controllers/student.js
@@ -8,29 +8,6 @@ studentsRouter.get('/', async (request, response) => {
 
     const students = await Student.findAll({
       attributes: ['id','forename', 'surname', 'student_code', 'email'],
-      include: [
-        {
-          model: Course,
-          attributes: ['id','title', 'years', 'code'],
-          as: 'courses',
-        },
-        {
-          model: Module,
-          attributes: ['id', 'title', 'semester', 'code'],
-          as: 'modules',
-          through: {
-            attributes: ['result', 'flagged'],
-          },
-          include: [
-            {
-              model: Course,
-              attributes: ['id'],
-              through: { attributes: [] },
-              as: 'course',
-            },
-          ],
-        },
-      ],
     })
 
 
@@ -38,35 +15,8 @@ studentsRouter.get('/', async (request, response) => {
       return response.status(404).json({ error: 'Student not found' })
     }
 
-    const formattedStudents = students.map((student) => ({
-      id: student.id,
-      forename: student.forename,
-      surname: student.surname,
-      student_code: student.student_code,
-      email: student.email,
-      courses: student.courses.map((course) => ({
-        id: course.id,
-        title: course.title,
-        years: course.years,
-        code: course.code,
-        modules: student.modules
-          .filter((module) =>
-            module.course.some((c) => c.id === course.id)
-          )
-          .map((module) => ({
-            id: module.id,
-            title: module.title,
-            semester: module.semester,
-            code: module.code,
-            result: module.student_module
-              ? module.student_module.dataValues.result
-              : 'Pending',
-            flagged: module.student_module.dataValues.flagged,
-          })),
-      }))
-    }))
 
-    response.json(formattedStudents)
+    response.json(students)
   } catch (error) {
     console.error('Error fetching student data:', error)
     response.status(500).json({ error: 'Internal server error' })
@@ -80,68 +30,14 @@ studentsRouter.get('/:student', async (request, response) => {
     const student = await Student.findOne({
       where: { id: studentId },
       attributes: ['id','forename', 'surname', 'student_code', 'email'],
-      include: [
-        {
-          model: Course,
-          attributes: ['id','title', 'years', 'code'],
-          as: 'courses',
-        },
-        {
-          model: Module,
-          attributes: ['id', 'title', 'semester', 'code'],
-          as: 'modules',
-          through: {
-            attributes: ['result', 'flagged'],
-          },
-          include: [
-            {
-              model: Course,
-              attributes: ['id'],
-              through: { attributes: [] },
-              as: 'course',
-            },
-          ],
-        },
-      ],
     })
 
     if (!student) {
       return response.status(404).json({ error: 'Student not found' })
     }
 
-    console.log('Student Modules:', student.modules)
 
-    const formattedStudent = {
-      student: {
-        id: student.id,
-        forename: student.forename,
-        surname: student.surname,
-        student_code: student.student_code,
-        email: student.email,
-      },
-      courses: student.courses.map(course => ({
-        id: course.id,
-        title: course.title,
-        years: course.years,
-        code: course.code,
-        modules: student.modules
-          .filter((module) =>
-            module.course.some((c) => c.id === course.id)
-          )
-          .map((module) => ({
-            id: module.id,
-            title: module.title,
-            semester: module.semester,
-            code: module.code,
-            result: module.student_module
-              ? module.student_module.dataValues.result
-              : 'Pending',
-            flagged: module.student_module.dataValues.flagged,
-          })),
-      }))
-    }
-
-    response.json(formattedStudent)
+    response.json(student)
   } catch (error) {
     console.error('Error fetching student data:', error)
     response.status(500).json({ error: 'Internal server error' })

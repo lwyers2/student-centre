bb8bb52616d2aff54caab9afea617a3a9a72f0f1
lwyers2 <lwyers2@gmail.com>
2025-01-25 18:39:54 +0000

    get all Modules api

--- backend/services/module.js
+++ backend/services/module.js
@@ -1,8 +1,51 @@
-const {Course, Module, User, ModuleYear, ModuleCourse, Semester, CourseYear, QualificationLevel } = require('../models')
+const { Course, Module, User, ModuleYear, ModuleCourse, Semester, CourseYear, QualificationLevel } = require('../models')
 
 async function getAllModules() {
   const modules = await Module.findAll({
-    attributes: ['id', 'title', 'code', 'year']
+    attributes: ['id', 'title', 'code', 'year', 'CATs'],
+    include: [
+      {
+        model: ModuleYear,
+        as: 'module_years',
+        attributes: ['id', 'year_start', 'semester_id'],
+        include: [
+          {
+            model: ModuleCourse,
+            as: 'module_courses',
+            attributes: ['id'],
+            include: [
+              {
+                model: Course,
+                as: 'course',
+                attributes: ['id', 'title', 'code', 'part_time', 'years'],
+                include: [
+                  {
+                    model: CourseYear,
+                    as: 'course_years',
+                    attributes: ['id', 'year_start', 'year_end'],
+                  },
+                  {
+                    model: QualificationLevel,
+                    as: 'qualification_level',
+                    attributes: ['qualification'],
+                  }
+                ]
+              },
+            ]
+          },
+          {
+            model: User,
+            as: 'module_co-ordinator',
+            attributes: ['forename', 'surname']
+          },
+          {
+            model: Semester,
+            as: 'semester',
+            attributes: ['id', 'name']
+          },
+        ],
+      },
+    ],
   })
   if(!modules) {
     return null
